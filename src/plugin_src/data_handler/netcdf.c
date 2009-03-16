2651425ef640fdee82393da5098db28110df52a7
ronni <ronni@1095105e-3e63-0410-b14e-63ce1eee999c>
2009-03-16 18:21:57 +0000

    changed 'degrees_lat/lon' to 'meters_lat/lon' as this is the actual unit right now.
    git-svn-id: svn://svn.gps.alaska.edu/crusde/trunk@2 1095105e-3e63-0410-b14e-63ce1eee999c

--- src/plugin_src/data_handler/netcdf.c
+++ src/plugin_src/data_handler/netcdf.c
@@ -60,8 +60,8 @@ static const char* netcdf_longitude 	= "longitude";
 static const char* netcdf_longname	= "experiment name";
 static const char* netcdf_time 		= "time";
 static const char* netcdf_direction	= "direction";
-static const char* netcdf_deg_east 	= "degrees_east";
-static const char* netcdf_deg_north 	= "degrees_north";
+static const char* netcdf_deg_east 	= "meters_east";
+static const char* netcdf_deg_north 	= "meters_north";
 static const char* netcdf_meters 	= "m";
 static const char* netcdf_days 		= "days";
 
