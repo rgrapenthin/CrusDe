dd55ce57ac47aaffe5c745097ea507a8be6c5c66
ronni <ronni@1095105e-3e63-0410-b14e-63ce1eee999c>
2009-03-23 03:11:20 +0000

    cleanup. removed files that do not belong in repository and renamed others so they are a little more intuitive.
    git-svn-id: svn://svn.gps.alaska.edu/crusde/trunk@4 1095105e-3e63-0410-b14e-63ce1eee999c

--- src/plugin_src/crustal_decay/defs.h
+++
@@ -1,8 +0,0 @@
-exponential.o: exponential.c \
-  /home/roon/projects/CrusDe-devel/trunk/src/crusde_api.h \
-  /home/roon/projects/CrusDe-devel/trunk/src/constants.h \
-  /home/roon/projects/CrusDe-devel/trunk/src/config.h
-exponential_rate.o: exponential_rate.c \
-  /home/roon/projects/CrusDe-devel/trunk/src/crusde_api.h \
-  /home/roon/projects/CrusDe-devel/trunk/src/constants.h \
-  /home/roon/projects/CrusDe-devel/trunk/src/config.h
--- src/plugin_src/load/defs.h
+++
@@ -1,8 +0,0 @@
-disk_load.o: disk_load.c \
-  /home/roon/projects/CrusDe-devel/trunk/src/crusde_api.h \
-  /home/roon/projects/CrusDe-devel/trunk/src/constants.h \
-  /home/roon/projects/CrusDe-devel/trunk/src/config.h
-irregular_load.o: irregular_load.c \
-  /home/roon/projects/CrusDe-devel/trunk/src/crusde_api.h \
-  /home/roon/projects/CrusDe-devel/trunk/src/constants.h \
-  /home/roon/projects/CrusDe-devel/trunk/src/config.h
